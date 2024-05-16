76a62f0facfe3efa8c5a462fbdcbbf8002f55197
ricardopra <ricardopra@yahoo.com.br>
2024-05-16 16:33:32 -0300

    commit

--- ws-java/curso_programacao/src/Main.java
+++ ws-java/curso_programacao/src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		int y = 32;
+		double x = 10.35784;
 		System.out.println("Hello World !");
-
+		System.out.println("Good Morning !");
+		System.out.println(y);
+		System.out.printf("%.2f%n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.2f%n", x);
 	}
 
 }
