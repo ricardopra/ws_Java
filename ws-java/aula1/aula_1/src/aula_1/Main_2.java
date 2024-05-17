d770a6eea78ec0a436e46375c4c63ce916d40f17
ricardopra <ricardopra@yahoo.com.br>
2024-05-17 10:57:42 -0300

    commit

---
+++ ws-java/aula1/aula_1/src/aula_1/Main_2.java
@@ -0,0 +1,15 @@
+package aula_1;
+
+public class Main_2 {
+
+	public static void main(String[] args) {
+		int a, b;
+		double resultado;
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado);
+
+	}
+
+}
