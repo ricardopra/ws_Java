0abadf7a1bf886c259e6421f5dd9a0f428d88d41
ricardopra <ricardopra@yahoo.com.br>
2024-05-22 09:38:49 -0300

    commit

---
+++ ws-java/aula1/aula_1/src/aula_1/Main4.java
@@ -0,0 +1,26 @@
+package aula_1;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		String x;
+		int y;
+		double z;
+		
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		sc.close();
+	
+	}
+
+}
